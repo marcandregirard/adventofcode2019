67386d90f6c4856e76cbf138711e584dacc4d91d
Marc-Andre Girard <Marc-Andre.Girard@iaah.ca>
2019-12-04 10:24:16 -0500

    Remove the class since it was not used as a class really.

--- src/day1/DayOne.kt
+++ src/day1/DayOne.kt
@@ -5,7 +5,7 @@ import java.io.File
 fun main(args: Array<String>) {
     val fuelsNeeded: ArrayList<Int> = ArrayList()
     val fuelsNeededForFuel: ArrayList<Int> = ArrayList()
-    File("src/day1/input_fuels.txt").forEachLine { fuelsNeeded.add(DayOne().calculateFuelByMass(it.toInt())) }
+    File("src/day1/input_fuels.txt").forEachLine { fuelsNeeded.add(calculateFuelByMass(it.toInt())) }
     print("Sums of fuel for all modules : ")
     val sumOfModules = fuelsNeeded.map { it }.sum()
     println(sumOfModules)
@@ -22,20 +22,18 @@ private fun calculateFuelForModule(it: Int, fuelsNeededForFuel: ArrayList<Int>) 
     var sums = 0
     var remainingFuel = it
     do {
-        remainingFuel = DayOne().calculateFuelByMass(remainingFuel)
+        remainingFuel = calculateFuelByMass(remainingFuel)
         sums += remainingFuel
     } while (remainingFuel != 0)
     fuelsNeededForFuel.add(sums)
 }
 
-class DayOne {
     // divide by 3, round down, substract 2
-    fun calculateFuelByMass(mass: Int): Int {
-        val roundedDownValue = mass / 3
-        val substractedValue = roundedDownValue - 2
-        if(substractedValue <= 0) {
-            return 0
-        }
-        return substractedValue
+fun calculateFuelByMass(mass: Int): Int {
+    val roundedDownValue = mass / 3
+    val substractedValue = roundedDownValue - 2
+    if(substractedValue <= 0) {
+        return 0
     }
+    return substractedValue
 }
